82a53b2a941c43692565fd862905265153f04540
bhanuteja <146553668+Bhanutejadomala@users.noreply.github.com>
2024-01-16 19:57:06 +0530

    Add files via upload

---
+++ calculator/script.js
@@ -0,0 +1,9 @@
+function demo(num){
+    document.getElementById('screen').value+=num
+}
+function demo2(){
+    document.getElementById('screen').value=eval(document.getElementById('screen').value)
+}
+function demo3(){
+    document.getElementById('screen').value=0
+}
